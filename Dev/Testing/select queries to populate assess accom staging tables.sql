5be2038a370201461ea831b4ae9bd8d160dedabf
George Giddens <github@georgegiddens.com>
2014-08-06 10:58:09 -0400

    Pull remote data into staging tables

---
+++ Dev/Testing/select queries to populate assess accom staging tables.sql
@@ -0,0 +1,27 @@
+
+--- create schema LEGACYACCOM
+
+
+select a.*
+from LEGACYSPED.MAP_IEPStudentRefID m 
+join Dev2005.QASCConvert2005.dbo.IEPAccomModTbl a on m.StudentRefID = a.GStudentID -- notice that intellisense not working here. That's why we use table variables, to leverage intellisense on queries below
+join Dev2005.QASCConvert2005.dbo.IEPAccomModTbl_SC a2 on a.IEPAccomSeq = a2.IEPAccomSeq 
+where isnull(a.del_flag,0)=0
+
+select a2.*
+from LEGACYSPED.MAP_IEPStudentRefID m 
+join Dev2005.QASCConvert2005.dbo.IEPAccomModTbl a on m.StudentRefID = a.GStudentID
+join Dev2005.QASCConvert2005.dbo.IEPAccomModTbl_SC a2 on a.IEPAccomSeq = a2.IEPAccomSeq 
+where isnull(a.del_flag,0)=0 and isnull(a2.del_flag,0)=0
+
+select aa.*
+from LEGACYSPED.MAP_IEPStudentRefID m 
+join Dev2005.QASCConvert2005.dbo.IEPAccomModTbl a on m.StudentRefID = a.GStudentID
+join Dev2005.QASCConvert2005.dbo.IEPAccomModTbl_SC a2 on a.IEPAccomSeq = a2.IEPAccomSeq 
+join Dev2005.QASCConvert2005.dbo.IEPAccomModListTbl_SC aa on a.IEPAccomSeq = aa.IEPAccomSeq 
+where isnull(a.del_flag,0)=0 and isnull(a2.del_flag,0)=0 and isnull(aa.del_flag,0)=0
+
+
+
+
+
