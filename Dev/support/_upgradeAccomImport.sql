c34cde3ae2f46bc7ec313aaef61a59bebcad9308
muthuv <muthuveerappan0104@gmail.com>
2014-08-06 15:58:39 -0600

    Added the sql file to run the scripts Need to create a bat file also

---
+++ Dev/support/_upgradeAccomImport.sql
@@ -0,0 +1,30 @@
+:r ..\Dev\Changes\0000-RegisterModule.sql
+Go
+:r ..\Dev\Changes\0001-Extract-EO_DistrictTestAccomm.sql
+Go
+:r ..\Dev\Objects\003-SetupETL.sql
+GO
+:r ..\Dev\Objects\Create MAP_TestNames.sql
+GO
+:r ..\Dev\Objects\create MAP_TestDefID.sql
+GO
+:r ..\Dev\Objects\Create_Map_table_FormInputvalue.sql
+GO
+:r ..\Dev\Objects\FormInputFieldsview.sql
+GO
+:r ..\Dev\Objects\LeagacyAccom_pivot.sql
+GO
+:r ..\Dev\Objects\Transform_FormInputSingleSelectValue.sql
+GO
+:r ..\Dev\Objects\Transform_PrgSectionFormInstance.sql
+GO
+:r ..\Dev\Objects\Transform_FormInputTextValue.sql
+GO
+:r ..\Dev\Objects\Transform_FormInputValue.sql
+GO
+:r ..\Dev\Objects\Populate_AccomData.sql
+GO
+:r ..\Dev\Objects\Populate_EO_DistrictTestAccomm.sql
+GO
+:r ..\Dev\Objects\PopulateAccomdationdata.sql
+GO
